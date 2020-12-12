c8b0cac58886907b4bbc003fe894aea5ef14246c
Bello Emmanuel Oluwatimilehin <Timilehin-bello@users.noreply.github.com>
2020-12-12 09:47:34 +0100

    Implemented HomePage Component  Product Listing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to Tosabella Stores </h1>
+          <HomePage />
         </Container>
       </main>
       <Footer />
