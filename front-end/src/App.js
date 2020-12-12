d6976ac7c1b87715247be4d25f9a79ad49145426
Bello Emmanuel Oluwatimilehin <Timilehin-bello@users.noreply.github.com>
2020-12-12 08:26:41 +0100

    Implemented Header and Footer Component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,17 @@
-import "./App.css";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Tosabella Stores </h1>
+    <div>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Tosabella Stores </h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
