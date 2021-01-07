5330ff2f43c406b66e01bca9f3a9896a61c95747
Bello Emmanuel Oluwatimilehin <Timilehin-bello@users.noreply.github.com>
2021-01-07 13:06:10 +0100

    Added colors to console

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
 import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
 dotenv.config();
 
 connectDB();
+
 const app = express();
 
 app.get("/", (req, res) => {
@@ -25,5 +27,7 @@ const PORT = process.env.PORT || 8080;
 
 app.listen(
   PORT,
-  console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`)
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
 );
