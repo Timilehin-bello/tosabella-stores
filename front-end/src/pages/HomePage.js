b43562d13df6e81891b5387885944a83779758cc
Bello Emmanuel Oluwatimilehin <Timilehin-bello@users.noreply.github.com>
2021-01-18 18:31:27 +0100

    Brought redux state into HomePage component

--- front-end/src/pages/HomePage.js
+++ front-end/src/pages/HomePage.js
@@ -1,30 +1,36 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
 import { Col, Row } from "react-bootstrap";
 import Product from "../components/Product ";
-import axios from "axios";
 
 function HomePage() {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
+  const dispatch = useDispatch();
 
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+  const productList = useSelector((state) => state.productList);
+
+  const { loading, error, products } = productList;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <div>
       <h1>Latest Product</h1>
-      <Row>
-        {products.map((product, i) => (
-          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading....</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product, i) => (
+            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </div>
   );
 }
