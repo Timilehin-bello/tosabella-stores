a9a077ead1e1743f4422b684793700de0aa76f89
Bello Emmanuel Oluwatimilehin <Timilehin-bello@users.noreply.github.com>
2021-01-22 20:28:46 +0100

    Implemented add to cart functionality

--- front-end/src/pages/CartPage.js
+++ front-end/src/pages/CartPage.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../actions/cartActions";
 
-function CartPage() {
-  return <div>CART</div>;
+function CartPage({ location, match }) {
+  const productId = match.params.id;
+
+  const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+
+  const { cartItems } = cart;
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+  return <div>CART </div>;
 }
 
 export default CartPage;
