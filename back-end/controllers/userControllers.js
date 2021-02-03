dd82be3a92a374a08af35ce324c9936ff64850d8
Bello Emmanuel Oluwatimilehin <Timilehin-bello@users.noreply.github.com>
2021-02-03 21:39:02 +0100

    Generated a JSON web token for user authentication

--- back-end/controllers/userControllers.js
+++ back-end/controllers/userControllers.js
@@ -1,6 +1,10 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
+import generateToken from "../utilis/generateToken.js";
 
+// @desc Auth user and get token
+//@route POST /api/users/login
+//@access Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
@@ -11,7 +15,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
